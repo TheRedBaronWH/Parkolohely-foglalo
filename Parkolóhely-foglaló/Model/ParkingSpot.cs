a2f7d73b4ac7dcbf98d47e3913ea40fdfe6d6e73
TheRedBaronWH <gomanpc@yahoo.com>
2026-08-04 15:37:16 +0200

    ParkingSpot and Reservation model

---
+++ Parkolóhely-foglaló/Model/ParkingSpot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parkolóhely_foglaló.Model;
+
+public class ParkingSpot
+{
+    public int Row { get; set; }
+    public int Column { get; set; }
+
+    public override string ToString()
+    {
+        return $"Parking spot at Row {Row} Column {Column}";
+    }
+}
---
+++ Parkolóhely-foglaló/Model/ParkingSpotReservation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parkolóhely_foglaló.Model;
+
+public class ParkingSpotReservation
+{
+    public ParkingSpot Spot { get; set; }
+    public DateTime StartingTime { get; set; }
+    public DateTime EndingTime { get; set; }
+    public string ReservedBy { get; set; }
+
+    public override string ToString()
+    {
+        return $"Reservation for {Spot} from {StartingTime} to {EndingTime} by {ReservedBy}";
+    }
+}
